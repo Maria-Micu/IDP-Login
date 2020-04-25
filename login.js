ec29398e19385b1a74c4feb6f0103421aa4f1a29
Maria Micu <maria.micu@stud.acs.upb.ro>
2020-04-26 00:43:53 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,6 @@
 const express = require('express')
 var bodyParser = require('body-parser')
+const axios = require("axios");
 const mysql = require('mysql2')
 
 const app = express()
@@ -7,21 +8,74 @@ const port = 5001
 
 const SERVICE_NAME = "Login";
 
+/*
+// Endpoints
+const MONITORING_URL = "http://monitoring-service:5005/log";
+// const GET_CART_URL = "http://cart-service:5002/cart/";
+const GET_CART_URL = "http://cart-service:5002/cart/";
+*/
+
 var promisePool;
 
 var jsonParser = bodyParser.json()
 
 app.post('/login', jsonParser, async function (req, res) {
     
-    const [result, fields] = await promisePool.query(`SELECT guid, user FROM login where user = '${req.body.username}' and password = '${req.body.password}'`)
+     postAsyncLog("Endpoint login called")
 
+    console.log("Username: " + req.body.username);
+    console.log("Password: " + req.body.password);
+
+    const [result, fields] = await promisePool.query(`SELECT guid, user FROM login where user = '${req.body.username}' and password = '${req.body.password}'`)
+    postAsyncLog(`Results fetched: ${result}`)
+
+    /*const cartDetails = await fetchCartForGuid(result[0].guid);
+
+    const respondeJson = {
+        'userDetails': result,
+        'cartDetails': cartDetails.data.cartDetails
+    }
+    */
     res.send(result);
 })
 
 app.get('/', (req, res) => res.send('It works!'))
 
+// Define http Method For generic use
+const postAsyncLog = async message => {
+    try {
+        params = {
+            service: SERVICE_NAME,
+            timestamp: Date.now(),
+            message: message,
+        }
+
+        const response = await axios.post(MONITORING_URL, params);
+        if (response.status == 200) {
+            console.log("Successfully sent to monitoring");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+/*
+const fetchCartForGuid = async guid => {
+    try {
+        const response = await axios.get(GET_CART_URL + guid);
+
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+};
+*/
+
 // Start server and establish connection to db
 app.listen(port, () => {
+
+    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Establish connection to db...`)
 
     const pool = mysql.createPool({
         host: 'database',
